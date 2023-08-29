445ebba806ef1d227659af852f839f3ad3a3c037
vikas143516 <vikas@tilicho.in>
2023-08-29 11:58:11 +0530

    Deleted manual yml

--- src/controllers/user/users.queries.js
+++ src/controllers/user/users.queries.js
@@ -15,10 +15,10 @@ exports.getListUsersQuery = (search_key, pageLimit, pageNumber) => {
     querySearchCondition = `WHERE first_name ILIKE '%${search_key}%' OR last_name ILIKE '%${search_key}%'`;
   }
   if (pageNumber || pageLimit) {
-    queryPagination = `OFFSET((${parseInt(
-      pageNumber ? pageNumber : 1,
-    )}-1)*${parseInt(pageLimit ? pageLimit : 10)})
-           ROWS FETCH NEXT ${parseInt(pageLimit ? pageLimit : 10)} ROWS ONLY`;
+    queryPagination = `OFFSET((${parseInt(pageNumber || 1)}-1)*${parseInt(
+      pageLimit || 10,
+    )})
+           ROWS FETCH NEXT ${parseInt(pageLimit || 10)} ROWS ONLY`;
   }
   const query = `select
     COUNT(*) OVER() AS data_count,
