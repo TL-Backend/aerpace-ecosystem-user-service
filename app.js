c0b5db4241198fb68e67a6254581c8d7541fe3bf
Pradeep-p73 <pradeep@tilicho.in>
2023-09-06 12:22:21 +0530

    added master pages dump function

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) =>
     req,
     res,
     message: 'Route not found',
-  })
+  }),
 );
 
 app.use((error, req, res, next) =>
@@ -30,7 +30,7 @@ app.use((error, req, res, next) =>
     res,
     error,
     message: error.message,
-  })
+  }),
 );
 
 app.listen(process.env.PORT || 3000, () => {
