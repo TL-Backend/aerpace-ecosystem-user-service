4d72e2f38abd04eb0da4ad3c4bbbcb0a6cd3bdba
Satyanarayana Raju <133110337+satyanarayanaRaju21@users.noreply.github.com>
2023-10-17 22:16:24 +0530

    made changes to temporary password

--- src/utils/passwordHandler.js
+++ src/utils/passwordHandler.js
@@ -1,29 +1,20 @@
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 
-exports.generateTemporaryPassword = async () => {
-  const characterSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  const digits = '0123456789';
-  const specialCharacters = '!@#$%^&*';
-  const allCharacters = characterSet + digits + specialCharacters;
-  const minimumLength = 8
-  const maximumLength = 12
-
-  const randomUppercase = characterSet[Math.floor(crypto.randomInt(characterSet.length))];
-  const randomDigit = digits[Math.floor(crypto.randomInt(digits.length))];
-  const randomSpecialCharacter = specialCharacters[Math.floor(crypto.randomInt(specialCharacters.length))];
-
-  const randomLength = Math.floor(crypto.randomInt((maximumLength - minimumLength + 1)) + minimumLength);
-
-  const randomString = Array.from({ length: randomLength }, () => {
-    const randomIndex = Math.floor(crypto.randomInt(allCharacters.length));
-    return allCharacters[randomIndex];
-  }).join('');
-
-  const temporaryPassword = randomString.replace(/[A-Z]/, randomUppercase)
-    .replace(/[0-9]/, randomDigit)
-    .replace(/[^A-Za-z0-9]/, randomSpecialCharacter);
-  return temporaryPassword
+exports.generateTemporaryPassword = async (length = 8) => {
+  const upperCaseCharset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowerCaseCharSet = 'abcdefghijklmnopqrstuvwxyz';
+  const numberSet = '0123456789'
+  let password = '@';
+  for (let index = 0; password.length < length; index++) {
+    const randomUpperCaseIndex = crypto.randomInt(0, upperCaseCharset.length);
+    const randomLowerCaseIndex = crypto.randomInt(0, lowerCaseCharSet.length);
+    const randomNumberIndex = crypto.randomInt(0, numberSet.length);
+    password += upperCaseCharset[randomUpperCaseIndex]
+      + lowerCaseCharSet[randomLowerCaseIndex]
+      + numberSet[randomNumberIndex];
+  }
+  return password;
 };
 
 exports.hashPassword = async ({ password }) => {
