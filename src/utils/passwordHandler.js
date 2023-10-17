d1daf140f73d7ec9bfdd6efbaff2e27d65e83f28
Satyanarayana Raju <133110337+satyanarayanaRaju21@users.noreply.github.com>
2023-10-17 14:09:59 +0530

    resolved sonar security issues

--- src/utils/passwordHandler.js
+++ src/utils/passwordHandler.js
@@ -1,27 +1,28 @@
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 
-exports.generateTemporaryPassword = async (length = 10) => {
-  const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+exports.generateTemporaryPassword = async () => {
+  const characterSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   const digits = '0123456789';
   const specialCharacters = '!@#$%^&*';
-  const allCharacters = uppercaseLetters + digits + specialCharacters;
+  const allCharacters = characterSet + digits + specialCharacters;
+  const minimumLength = 8
+  const maximumLength = 12
 
-  const randomUppercase = uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
-  const randomDigit = digits[Math.floor(Math.random() * digits.length)];
-  const randomSpecialCharacter = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+  const randomUppercase = characterSet[Math.floor(crypto.randomInt(characterSet.length))];
+  const randomDigit = digits[Math.floor(crypto.randomInt(digits.length))];
+  const randomSpecialCharacter = specialCharacters[Math.floor(crypto.randomInt(specialCharacters.length))];
 
-  const randomLength = Math.floor(Math.random() * (12 - 8 + 1)) + 8;
+  const randomLength = Math.floor(crypto.randomInt((maximumLength - minimumLength + 1)) + minimumLength);
 
   const randomString = Array.from({ length: randomLength }, () => {
-    const randomIndex = Math.floor(Math.random() * allCharacters.length);
+    const randomIndex = Math.floor(crypto.randomInt(allCharacters.length));
     return allCharacters[randomIndex];
   }).join('');
 
   const temporaryPassword = randomString.replace(/[A-Z]/, randomUppercase)
     .replace(/[0-9]/, randomDigit)
     .replace(/[^A-Za-z0-9]/, randomSpecialCharacter);
-  console.log(temporaryPassword)
   return temporaryPassword
 };
 
